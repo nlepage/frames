785e48b612738504910fe534fcf34d2c02c8c4d5
nlepage <19571875+nlepage@users.noreply.github.com>
2019-01-30 11:37:16 +0100

    :sparkles: Add docs for gh pages

---
+++ docs/precache-manifest.5a1fcbddc42ad1152856b4eb7ac1a9e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b45474c68eab99c9d67c",
+    "url": "/frames/static/css/main.5a887c77.chunk.css"
+  },
+  {
+    "revision": "b45474c68eab99c9d67c",
+    "url": "/frames/static/js/main.b45474c6.chunk.js"
+  },
+  {
+    "revision": "91f11a769c46db6e043e",
+    "url": "/frames/static/css/1.35bc20d1.chunk.css"
+  },
+  {
+    "revision": "91f11a769c46db6e043e",
+    "url": "/frames/static/js/1.91f11a76.chunk.js"
+  },
+  {
+    "revision": "f1ed0606214ee1dc3659",
+    "url": "/frames/static/js/runtime~main.f1ed0606.js"
+  },
+  {
+    "revision": "32aa77d1833a4259d5dddfd0694ca87f",
+    "url": "/frames/index.html"
+  }
+];
